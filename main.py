3f04fb1fabe7d247a94dcb0786e1386ca4ab3952
Nicholas <nihu21@student.bth.se>
2026-02-12 11:34:54 +0100

    main refactored and gitignore slightly updated

--- main.py
+++ main.py
@@ -7,15 +7,31 @@ load_dotenv()
 
 TOKEN = os.getenv("GITHUB_TOKEN")
 
-url = "https://api.github.com/search/code?q=@context+openvex+author+@id+statements+timestamp+in:file+language:json"
-
 headers = {
   'Authorization': f'Token {TOKEN}'
 }
 
 openvex = ["@context", "openvex", "author", "@id", "statements", "timestamp"]
 
-def get_vex_spec_files(spec: list, specname: str, filetype: str) -> None: 
+def download_file(file_urls: list, folder: str):
+    file_id = 0
+    for url in file_urls:
+        url = url.replace("https://", "https://raw.")
+        url = url.replace("blob/", "")
+
+        filename = url.split("/")[-1]
+        filename = str(file_id) + "_" + filename
+        filepath = f"{folder}/{filename}"
+
+        response = requests.request("GET", url, headers=headers)
+
+        with open(filepath, "w") as fp:
+            fp.write(response.content.decode("utf-8"))
+
+        file_id += 1
+
+#todo: add ability to specify date for code search
+def get_vex_spec_files(spec: list, specname: str, filetype) -> None: 
 
     url = "https://api.github.com/search/code?q="
 
@@ -26,11 +42,11 @@ def get_vex_spec_files(spec: list, specname: str, filetype: str) -> None:
     for kword in spec:
         url = url + kword + "+"
     
-    url = url + "in:file+language:" + filetype + "&per_page=100"
+    url = url + "in:file+extension:" + filetype + "&per_page=100"
 
     file_urls = []
 
-    for i in range(1, 10):
+    for i in range(1, 2):
         try:
             url_page = url + f"&page={i}"
             response = requests.request("GET", url_page, headers=headers)
@@ -40,37 +56,8 @@ def get_vex_spec_files(spec: list, specname: str, filetype: str) -> None:
             print("GitHub API response: ")
             print(response)
     
-    print(len(file_urls))
+    download_file(file_urls, folder)
     
-
 if __name__ == "__main__":
     get_vex_spec_files(openvex, "openvex", "json")
-
-# vex_folder = "vex_files"
-# if not os.path.exists(vex_folder):
-#     os.makedirs(vex_folder)
-
-# response = requests.request("GET", url, headers=headers)
-
-# data = response.json()
-
-# with open("data.json", "w") as f:
-#     json.dump(data, f)
-    
-# with open("data.json", "r") as f: 
-#     data = json.load(f)
-
-# with open("html_urls.txt", "w") as f:    
-#     counter = 0
-#     for item in data["items"]:
-#         f.write(item["html_url"] + "\n")
-#         url = item["html_url"]
-#         rawurl = url.replace("https://", "https://raw.")
-#         rawurl = rawurl.replace("blob/", "")
-#         response = requests.request("GET", rawurl, headers=headers)   
-#         filename = url.split("/")[-1]
-#         filename = str(counter) + "_" + filename
-#         filepath = f"vex_files/{filename}"
-#         with open(filepath, "w") as fp:
-#             json.dump(response.json(), fp)
-#         counter += 1
+    get_vex_spec_files(openvex, "openvex", "ct")
