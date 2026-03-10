38209dfcfd263d486ce46a9b6100b6df63f053b9
Nicholas <nihu21@student.bth.se>
2026-03-10 12:09:27 +0100

    working on reading from search_terms file

--- github_api/main.py
+++ github_api/main.py
@@ -40,8 +40,13 @@ specs = {
     }
 }
 
-def download_file(file_urls: list, folder: str) -> None:
-    file_id = 0
+def download_file(file_urls: list, folder: str, response_total) -> None:
+    
+    with open(f"{folder}/0_results_total.json", "w") as r:
+        json.dump(response_total.json(), r)
+
+    file_id = 1
+
     for url in file_urls:
         url = url.replace("https://", "https://raw.")
         url = url.replace("blob/", "")
@@ -70,11 +75,8 @@ def get_vex_spec_files(spec: list, specname: str, filetype: str) -> None:
     
     url = url + "in:file+extension:" + filetype + "&per_page=100&page=1"
 
-    response = requests.request("GET", url, headers=headers)
-    print(response)
-
-    with open("results.json", "w") as r:
-        json.dump(response.json(), r)
+    response_total = requests.request("GET", url, headers=headers)
+    # print(response_total)
 
     file_urls = []
     commit_urls = []
@@ -96,7 +98,7 @@ def get_vex_spec_files(spec: list, specname: str, filetype: str) -> None:
         for item in items:
             file_urls.append(item["html_url"])
 
-    download_file(file_urls, folder)
+    #download_file(file_urls, folder, response_total)
     #get_commit_history(commit_urls)
 
 def iterate_get_vex_spec(specs: dict) -> dict:
@@ -107,7 +109,7 @@ def iterate_get_vex_spec(specs: dict) -> dict:
             url = "https://api.github.com/search/code?q="
 
             for kword in specs[key]["keywords"]:
-                url = url + kword + "+"
+                url = url + kword + "&"
             
             url = url + "in:file+extension:" + extension + "&per_page=100&page=1"
             try:
@@ -133,7 +135,42 @@ def try_database() -> None:
     except:
         print("failed to connect")
 
+def get_search_term(spec: str) -> dict:
+    with open("github_api/search_terms.json", "r") as st:
+        search_terms = json.loads(st.read())
+        return search_terms[spec]
+    
+def get_all_search_terms() -> list:
+    with open("github_api/search_terms.json", "r") as st:
+        search_terms = json.loads(st.read())
+        return search_terms.keys()
+    
+def parse_search_term(spec_name: str, search_term: dict, include_optional=False) -> None:
+    # need specname: str, keywords: list, filetype: str
+    for filetype in search_term.keys():
+            extensions = search_term[filetype]['extentions']
+            keywords = search_term[filetype]['keywords']['required']
+            if include_optional:
+                keywords += search_term[filetype]['keywords']['optional']
+            print(
+f"""{spec_name}: \n
+    Filetype: {filetype}\n     
+    Required: {search_term[filetype]['keywords']['required']}\n
+    Optional: {search_term[filetype]['keywords']['optional']}\n
+    Extensions: {search_term[filetype]['extentions']}
+""")
+            for extension in extensions:
+                print(extension, keywords, spec_name)
+
+def iterate_all_search_terms() -> None:
+    keys = get_all_search_terms()
+    for key in keys:
+        parse_search_term(key, get_search_term(key), False)
+
 if __name__ == "__main__":
     #print(iterate_get_vex_spec(specs))
-    get_vex_spec_files(specs["openvex"]["keywords"], "OpenVex", "json")
+    #get_vex_spec_files(specs["csaf"]["keywords"], "csaf", "json")
     #try_database()
+    #parse_search_term("CycloneDX", get_search_term("CycloneDX"), True)
+    iterate_all_search_terms()
+    #print(get_search_term("CycloneDX"))
