41875a972b3b7b0c37896896d12af538fea0996f
Sofia Blom (Ned) <sofia@familjenblom.se>
2026-04-23 14:25:25 +0200

    Using Style the way it was meant to!
    I figured it out finally

--- github_api/analysis/Tools.py
+++ github_api/analysis/Tools.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import pandas as pd
-# from pandas.io.format.style import Styler
 import seaborn as sns
 from lxml import etree
 
@@ -81,8 +80,6 @@ def tools_tables(buckets: dict, file_count: dict, folder: Path) -> None:
     file_names = ["count_tools.tex"]
     contense = []
 
-    # base_styler = pd.io.formats.style.Styler(precision=2, decimal=",", thousands=" ", escape="latex")
-
     # Percentage of tools
     tools_vs_non_tools = {}
     for specification in buckets:
@@ -92,15 +89,18 @@ def tools_tables(buckets: dict, file_count: dict, folder: Path) -> None:
         }
     tools_vs_non_tools_df = pd.DataFrame(data=tools_vs_non_tools)
     tools_vs_non_tools_df = tools_vs_non_tools_df.transpose()
-    styler = pd.io.formats.style.Styler(data=tools_vs_non_tools_df, precision=2, decimal=",", thousands=" ", escape="latex")
+    styler = tools_vs_non_tools_df.style.format(precision=2, decimal=",", thousands=" ", escape="latex")
+    # styler = pd.io.formats.style.Styler(data=tools_vs_non_tools_df, precision=2, decimal=",", thousands=" ", escape="latex")
     contense.append(styler.to_latex(position_float="centering", label="Tools proportion", caption="Table detailing the proportion of files generated with a tool", hrules=True))
     # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.io.formats.style.Styler.to_latex.html#pandas.io.formats.style.Styler.to_latex instead?
 
     # Map buckets
     tools = pd.DataFrame(buckets)
-    styler = pd.io.formats.style.Styler(data=tools, precision=2, decimal=",", thousands=" ", escape="latex")
+    CycloneDX_tools = tools.dropna(subset=["CycloneDX"])
+    # CycloneDX_tools.drop(["SPDX", "OpenVEX", "CSAF"], inplace=True)
+    styler = CycloneDX_tools.style.format(precision=2, decimal=",", thousands=" ", escape="latex")
     file_names.append("Tools_in_use.tex")
-    contense.append(styler.to_latex(position_float="centering", label="Tools", caption="Table naming all the tools used", hrules=True))
+    contense.append(styler.to_latex(environment="longtable", label="Tools", caption="Table naming all the tools used", hrules=True))
 
     for file_name, content in zip(file_names, contense):
         filepath = folder / file_name
