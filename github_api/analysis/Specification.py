da182894901a2ab13e07e53ab4e024bf03ec7dbc
Sofia Blom (Ned) <sofia@familjenblom.se>
2026-04-27 11:39:43 +0200

    Fix spelling misstake

--- github_api/analysis/Specification.py
+++ github_api/analysis/Specification.py
@@ -3,11 +3,11 @@ from lxml import etree
 from .extentions import Extentions
 
 
-def spesification_analysis(
+def specification_analysis(
     vex, extention: Extentions, specification: str, buckets: dict
 ) -> dict:
     """
-    Extracts the version number of the spesification
+    Extracts the version number of the specification
 
     Parameters
     vex - the Vex file
@@ -18,36 +18,38 @@ def spesification_analysis(
     Returns
     buckets
     """
-    found_spesification = False
+    found_specification = False
     if extention == Extentions.JSON:
         if specification == "OpenVEX" and "@context" in vex.keys():
             context = vex["@context"]
             version = context.replace("https://openvex.dev/ns/v", "")
             buckets[specification][version] += 1
-            found_spesification = True
+            found_specification = True
 
         elif specification == "CSAF" and "document" in vex.keys():
             buckets[specification][vex["document"]["csaf_version"]] += 1
-            found_spesification = True
+            found_specification = True
 
         elif specification == "CycloneDX" and "specVersion" in vex.keys():
 
             buckets[specification][vex["specVersion"]] += 1
-            found_spesification = True
+            found_specification = True
 
         elif specification == "SPDX" and "@graph" in vex.keys():
             for entry in vex["@graph"]:
                 if entry["type"] == "CreationInfo" and "specVersion" in entry.keys():
                     buckets[specification][entry["specVersion"]] += 1
-                    found_spesification = True
+                    found_specification = True
 
     elif extention == Extentions.XML:
         if specification == "CycloneDX":
             namespace = etree.QName(vex.tag).namespace
             version = namespace.replace("http://cyclonedx.org/schema/bom/", "")
             buckets[specification][version] += 1
-            found_spesification = True
+            found_specification = True
 
-    if found_spesification:
+    if found_specification:
         buckets[specification]["count"] += 1
     return buckets
+
+def 
