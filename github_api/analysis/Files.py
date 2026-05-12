7963ee616aa92b65c474056a13c430d6e9df2068
Sofia Blom (Ned) <sofia@familjenblom.se>
2026-05-12 11:25:17 +0200

    Files table now also shown the total amount of repos

--- github_api/analysis/Files.py
+++ github_api/analysis/Files.py
@@ -1,24 +1,30 @@
 import pandas as pd
 from pathlib import Path
 
-def files_table(file_counts: dict, total_documents: int, folder: Path) -> None:
+def files_table(file_counts: dict, repos: dict, total_documents: int, folder: Path) -> None:
     file_names = []
     content = []
 
     # files = pd.DataFrame(file_counts)
     files = []
+    repo_count = 0
     for specification in file_counts:
         if specification == "name":
             continue
+        repo = len(repos[specification].keys())
+        repo_count += repo
         files.append({
             "specification": specification,
             "count": file_counts[specification],
             "percentage": file_counts[specification] / total_documents,
+            "repositories": repo,
         })
+    files = sorted(files, key=lambda d: d["count"], reverse=True)
     files.append({
         "specification": "Total",
         "count": total_documents,
         "percentage": total_documents / total_documents,
+        "repositories": repo_count,
     })
     files_df = pd.DataFrame(files)
     files_df.set_index("specification", inplace=True)
@@ -29,7 +35,7 @@ def files_table(file_counts: dict, total_documents: int, folder: Path) -> None:
     content.append(
         styler.to_latex(
             environment="longtable",
-            column_format="p{10cm}r",
+            column_format="lrrr",
             label="tab:Files distribution",
             caption="Table detailing the proportion of files over the different specifications",
             hrules=True,
